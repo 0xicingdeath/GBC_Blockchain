2ab23acb13931241f0920ff86e7bb4638bd12f2d
Natalie Chin <chinn@mcmaster.ca>
2019-04-11 20:40:36 -0400

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function sayHello(name) {
+    return ("Hii " + name);
+}
+
+function sayGoodbye(name) {
+    return ("Bye: " + name);
+}
+
+module.exports = { sayHello, sayGoodbye }
---
+++ useTest.js
@@ -0,0 +1,11 @@
+let something = require('./index.js');
+console.log(something);
+
+var hello = something.sayHello("name");
+var bye = something.sayGoodbye("name");
+
+console.log(hello);
+console.log(bye);
+
+
+
