dfc2c13b1a4e783f8e169e0a91a2957ead2a9581
Natalie Chin <chinn@mcmaster.ca>
2019-04-16 21:41:35 -0400

    Cleaned up repo

--- useTest.js
+++ useTest.js
@@ -1,11 +1,4 @@
 let something = require('./index.js');
-console.log(something);
 
 var hello = something.sayHello("name");
 var bye = something.sayGoodbye("name");
-
-console.log(hello);
-console.log(bye);
-
-
-
