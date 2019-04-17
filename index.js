93b2aefefce4bf30629211d1b67ebd14bf0f6da0
Natalie Chin <chinn@mcmaster.ca>
2019-04-16 20:41:13 -0400

    bumped version and updated index

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ function sayHelloInFrench(name) {
     return ("Bonjour " + name);
 }
 
+function sayHelloWorld {
+    return "Hello World";
+}
+
 module.exports = { sayHello, sayGoodbye, sayHelloInFrench }
